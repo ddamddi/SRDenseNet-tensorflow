ad26a5490315ab11764236fcc7905ba0069f00a8
ddamddi <leonardkkh@gmail.com>
2020-07-20 22:30:07 +0900

    change cv2 to PIL

--- utils.py
+++ utils.py
@@ -3,8 +3,25 @@ import numpy as np
 import random
 import math
 import h5py
-import cv2
 import os
+from PIL import Image
+
+
+''' Invert Color Channel '''
+def rgb2ycbcr(img):
+    cvt_img = img.convert('YCbCr') 
+    return cvt_img
+
+def ycbcr2rgb(img):
+    cvt_img = img.convert('RGB')
+    return cvt_img
+
+'''  '''
+def imsave(img, filename):
+    img.save(filename, format='BMP')
+
+def imread(img_dir):
+    return Image.open(img_dir)
 
 def time_calculate(sec):
     s = sec % 60
@@ -17,171 +34,137 @@ def show_all_variables():
     model_vars = tf.trainable_variables()
     tf.contrib.slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
-''' For Load Datasets '''
-def load_291(scale=4):
-    filename = '291_' + str(scale) + 'x.h5'
-    file_dir = os.path.join(os.getcwd(), 'data', 'Train', filename)
-
-    with h5py.File(file_dir, 'r') as f:
-        label = list(f['label'])
-        data = list(f['data'])
-
-    label, data = _shuffle((label, data))
-    return label, data
-
-def load_DIV2K(scale=4):
-    filename = 'DIV2K_' + str(scale) + 'x.h5'
-    file_dir = os.path.join(os.getcwd(), 'data', 'Train', filename)
-
-    with h5py.File(file_dir, 'r') as f:
-        label = list(f['label'])
-        data = list(f['data'])
-
-    label, data = _shuffle((label, data))
-    return label, data
-
-def load_set5(scale=4, color_space='y'):
+''' For load training datasets '''
+def load_train(color_space='ycbcr'):
     color_space = color_space.lower()
-    GT_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set5', 'gt')
-    ILR_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set5', 'bicubic_' + str(scale) + 'x')
-
-    label = []
-    data = []
-
-    for img in os.listdir(GT_DIR):
-        IMG_PATH = os.path.join(GT_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            label.append(read_img[:,:,2:3])
-        else:
-            label.append(read_img)
+    DATA_DIR = os.path.join(os.getcwd(), 'data', 'Train', 'DIV2K_train_HR')
+    images = []
     
-    for img in os.listdir(ILR_DIR):
-        IMG_PATH = os.path.join(ILR_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            data.append(read_img[:,:,2:3])
-        else:
-            data.append(read_img)
+    for img in os.listdir(DATA_DIR):
+        image = imread(os.path.join(DATA_DIR, img))
+        if color_space == 'ycbcr':
+            image = rgb2ycbcr(image)
+        images.append(image)
+    return images[:500]
 
-    return _normalize(label), _normalize(data)
-
-def load_set14(scale=4, color_space='y'):
+''' For load testing datasets '''
+def load_set5(color_space='ycbcr'):
     color_space = color_space.lower()
-    GT_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set14', 'gt')
-    ILR_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set14', 'bicubic_' + str(scale) + 'x')
-
-    label = []
-    data = []
-
-    for img in os.listdir(GT_DIR):
-        IMG_PATH = os.path.join(GT_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            label.append(read_img[:,:,2:3])
-        else:
-            label.append(read_img)
+    DATA_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set5')
+    images = []
     
-    for img in os.listdir(ILR_DIR):
-        IMG_PATH = os.path.join(ILR_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            data.append(read_img[:,:,2:3])
-        else:
-            data.append(read_img)
+    for img in os.listdir(DATA_DIR):
+        if img[-3:] == 'bmp':
+            image = imread(os.path.join(DATA_DIR, img))
+            if color_space == 'ycbcr':
+                image = rgb2ycbcr(image)
+            images.append(image)
+    return images
 
-    return _normalize(label), _normalize(data)
-
-def load_b100(scale=4, color_space='y'):
+def load_set14(color_space='ycbcr'):
     color_space = color_space.lower()
-    GT_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'B100', 'gt')
-    ILR_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'B100', 'bicubic_' + str(scale) + 'x')
-
-    label = []
-    data = []
-
-    for img in os.listdir(GT_DIR):
-        IMG_PATH = os.path.join(GT_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            label.append(read_img[:,:,2:3])
-        else:
-            label.append(read_img)
+    DATA_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Set14')
+    images = []
     
-    for img in os.listdir(ILR_DIR):
-        IMG_PATH = os.path.join(ILR_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            data.append(read_img[:,:,2:3])
-        else:
-            data.append(read_img)
+    for img in os.listdir(DATA_DIR):
+        if img[-3:] == 'bmp':
+            image = imread(os.path.join(DATA_DIR, img))
+            if color_space == 'ycbcr':
+                image = rgb2ycbcr(image)
+            images.append(image)
+    return images
 
-    return _normalize(label), _normalize(data)
-
-def load_urban100(scale=4, color_space='y'):
+def load_b100(color_space='ycbcr'):
     color_space = color_space.lower()
-    GT_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Urban100', 'gt')
-    ILR_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Urban100', 'bicubic_' + str(scale) + 'x')
+    DATA_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'B100')
+    images = []
+    
+    for img in os.listdir(DATA_DIR):
+        if img[-3:] == 'bmp':
+            image = imread(os.path.join(DATA_DIR, img))
+            if color_space == 'ycbcr':
+                image = rgb2ycbcr(image)
+            images.append(image)
+    return images
 
-    label = []
+def load_urban100(color_space='ycbcr'):
+    color_space = color_space.lower()
+    DATA_DIR = os.path.join(os.getcwd(), 'data', 'Test', 'Urban100')
+    images = []
+    
+    for img in os.listdir(DATA_DIR):
+        if img[-3:] == 'bmp':
+            image = imread(os.path.join(DATA_DIR, img))
+            if color_space == 'ycbcr':
+                image = rgb2ycbcr(image)
+            images.append(image)
+    return images
+
+''' preprocessing '''
+def preprocessing(x):
+    x = to_array(x)
+    x = x[:,:,:,0:1] # get only Y channel
+    x = normalize(x)
+    x = shuffle(x)
+    return x
+
+def prepare_patches(x, patch_size=100, stride=25):
+    patches = []
+    for idx in range(len(x)):
+        img = x[idx]
+        h, w = img.size
+        h_cnt = (h - patch_size) // stride + 1
+        w_cnt = (w - patch_size) // stride + 1    
+
+        for i in range(h_cnt):
+            for j in range(w_cnt):
+                img_crop = img.crop((i*stride, j*stride, patch_size+i*stride, patch_size+j*stride))
+                patches.append(img_crop)
+
+    return patches
+
+def bicubic_downsampling(x, scale=4):
+    if not isinstance(x, list):
+        x = [x]
+    
+    bicubic = []
+    for idx in range(len(x)):
+        img = x[idx]
+        h, w = img.size
+        output_shape = (h//scale, w//scale)
+        img = img.resize(output_shape, Image.BICUBIC)
+        bicubic.append(img)
+    
+    return bicubic
+
+def bicubic_upsampling(x, scale=4):
+    if not isinstance(x, list):
+        x = [x]
+    
+    bicubic = []
+    for idx in range(len(x)):
+        img = x[idx]
+        h, w = img.size
+        output_shape = (h*scale, w*scale)
+        img = img.resize(output_shape, Image.BICUBIC)
+        bicubic.append(img)
+    
+    return bicubic
+
+def to_array(x):
     data = []
+    for idx in range(len(x)):
+        img = x[idx]
+        data.append(np.array(img))
+    return np.array(data)
 
-    for img in os.listdir(GT_DIR):
-        IMG_PATH = os.path.join(GT_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            label.append(read_img[:,:,2:3])
-        else:
-            label.append(read_img)
-    
-    for img in os.listdir(ILR_DIR):
-        IMG_PATH = os.path.join(ILR_DIR, img)
-        read_img = cv2.imread(IMG_PATH, cv2.IMREAD_COLOR)
-        if color_space == 'y':
-            data.append(read_img[:,:,2:3])
-        else:
-            data.append(read_img)
-
-    return _normalize(label), _normalize(data)
-
-''' For Dataset Pre-processing'''
-def create_sub_patches(x, patch_size=100, stride=50):
-    lr_patch_size = int(patch_size/4)
-    (HR, LR) = x
-    HR_patches = []
-    LR_patches = []
-
-    for idx in range(len(HR)):
-        HR_img = HR[idx]
-        LR_img = LR[idx]
-        row = HR_img.shape[0]
-        col = HR_img.shape[1]
-        row_cnt = row//patch_size
-        col_cnt = col//patch_size
-
-        for i in range(row_cnt):
-            for j in range(col_cnt):
-                HR_img_crop = HR_img[i*patch_size:(i+1)*patch_size, j*patch_size:(j+1)*patch_size]
-                LR_img_crop = LR_img[i*lr_patch_size:(i+1)*lr_patch_size, j*lr_patch_size:(j+1)*lr_patch_size]
-
-                HR_patches.append(HR_img_crop)
-                LR_patches.append(LR_img_crop)
-
-    return HR_patches, LR_patches
-
-def _shuffle(x):
-    (HR, LR) = x
-    
+def shuffle(x):
     seed = 777
     np.random.seed(seed)
-    np.random.shuffle(HR)
+    np.random.shuffle(x)
+    return x
 
-    np.random.seed(seed)
-    np.random.shuffle(LR)
-
-    return HR, LR
-
-def _normalize(x):
+def normalize(x):
     return np.array(x) / 255.
 
 def denormalize(x):
@@ -190,17 +173,11 @@ def denormalize(x):
     x = x.astype('uint8')
     return np.array(x)
 
-''' Invert Color Channel '''
-def bgr2ycrcb(img):
-    cvt_img = cv2.cvtColor(x[idx], cv2.COLOR_BGR2YCrCb) 
-    return cvt_img
+def mod_crop(x, scale):
+    h, w = x.size
+    cropped = x.crop((0, 0, h - h%scale, w - w%scale))
+    return cropped
 
-def ycrcb2bgr(img):
-    cvt_img = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
-    return cvt_img
-
-
-if __name__ == '__main__' :
-    label, data = load_291()
-    print(np.array(label).shape)
-    print(np.array(data).shape)
+if __name__ == '__main__':
+    x = load_train()
+    print(len(create_sub_patches(x[:500], 100, 100)))
